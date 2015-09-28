29a6dc05543c19e57300d401fda4df6d668cdfd1
Chris Brundage <chris.brundage@thechive.com>
2015-09-27 21:41:04 -0500

    The SelectDatabaseView had the wrong window title.

--- DailyStatements/Properties/AssemblyInfo.cs
+++ DailyStatements/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.7.0")]
-[assembly: AssemblyFileVersion("1.0.7.0")]
+[assembly: AssemblyVersion("1.0.8.0")]
+[assembly: AssemblyFileVersion("1.0.8.0")]
 [assembly: GuidAttribute("63D760C7-265D-47D4-A693-1A56316899AF")]
